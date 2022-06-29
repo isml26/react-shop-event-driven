f3fb82f3bb0a737d34bc256e9815cc3fbe87e89e
ismail26 <iguven1999@hotmail.com>
2022-06-29 17:03:46 +0300

    shop routes,shop page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,9 +7,9 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc,collection,writeBatch,query,getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBn0FrlZxR7s8OPkt8LPjBsa5A1qIML71c",
@@ -22,6 +22,33 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+export const addCollectionAndDocuments = async(collectionKey,objectsToAdd)=>{
+  const collectionRef = collection(db,collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object)=>{
+    const docRef = doc(collectionRef,object.title.toLowerCase());
+    batch.set(docRef,object);
+  });
+  await batch.commit();
+  console.log('Done');
+}
+
+export const getCategoriesAndDocuments = async()=>{
+  const collectionRef = collection(db,'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  
+  const categoryMap = querySnapshot.docs.reduce((acc,docSnapshot)=>{
+
+    const {title,items} = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  },{})
+  return categoryMap;
+}
 
 const googleProvider = new GoogleAuthProvider();
 
